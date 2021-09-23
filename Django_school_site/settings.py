b08127f5b93797753383dd3f532b6b91733b8f2d
ARCHITECT <greatraksin@icloud.com>
2021-09-23 20:30:14 +0300

    Fixing settings

--- Django_school_site/settings.py
+++ Django_school_site/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'verho203pd3rwfe' or os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -141,3 +141,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'production_media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# OYlPBjYvuk
