f4feee993f8b20910b8414ed06a6361f9eee63b9
100ballovby <legorobotsbel@gmail.com>
2022-06-29 21:12:14 +0300

    Adding post annotation

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ from tinymce import models as tinymce_models
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    annotation = tinymce_models.HTMLField('Краткий текст', max_length=3000, blank=True, null=True, db_index=True)
     text = tinymce_models.HTMLField('Текст поста', max_length=7000, blank=True, null=True, db_index=True)
     created_date = models.DateTimeField(default=timezone.now)
     is_active = models.BooleanField('Опубликовано', db_index=True, blank=True, null=True, default=False)
