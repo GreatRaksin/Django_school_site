fc8f8c3ed934a359f680a2fdfdba8a506a7589ec
100ballovby <legorobotsbel@gmail.com>
2022-06-29 20:02:28 +0300

    Adding Post model for blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
+from tinymce import models as tinymce_models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = tinymce_models.HTMLField('Текст поста', max_length=7000, blank=True, null=True, db_index=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField('Опубликовано', db_index=True, blank=True, null=True, default=False)
+
+    class Meta:
+        ordering = ['created_date']
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
